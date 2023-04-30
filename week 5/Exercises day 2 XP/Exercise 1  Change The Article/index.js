92e2428c16fdc2838845a929b8b90ecf939e31b6
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-04-30 15:37:25 +0300

    changes

--- week 5/Exercises day 2 XP/Exercise 1  Change The Article/index.js
+++ week 5/Exercises day 2 XP/Exercise 1  Change The Article/index.js
@@ -21,91 +21,76 @@
 
 
 
-const elem = document.getElementsByName('h2')[0]
-elem.addEventListener('click',function(e){
-    e.target.style.backgroundColor = 'red'
-})
+//const elem = document.getElementsByName('h2')[0]
+//elem.addEventListener('click',function(e){
+   // e.target.style.backgroundColor = 'red'
+//})
 
 
 
 
+  const h1 = getFirstEelment("h1")
+  const h2 = getFirstEelment("h2")
+  const h3 = getFirstEelment("h3")
+  const secondParagraph = getSecondParagraph()
+
+  removeLastLine()
+  addButton()
+
+   h1?.addEventListener("mouseover", randomizeH1size)
+   h2?.addEventListener("click", changeBackground)
+   h3?.addEventListener("click", hideH3)
+   secondParagraph.addEventListener("mouseover", makeSecondParagraphFadeout)
+
+function makeSecondParagraphFadeout(){
+   secondParagraph.classList.add("fadeout") 
+}
+
+ function getSecondParagraph(){
+   return document.querySelectorAll("p")[1]
+
+}
 
 
+ function getRandomNumber(){
+    return Math.floor(Math.random () * 100 + 1 )
+}
+
+function randomizeH1size (){
+   const randomNumber = getRandomNumber()
+    h1.style .fontSize = randomNumber + "px" 
+}
+ function makeParagraphsBold(){
+   const paragraphs = document.querySelectorAll("p")
+    for (const paragraph of paragraphs){
+        paragraph.classList.add("Blob")
+   }
+ }
 
 
+ function addButton(){
+    const button = document.createElement("button")
+    button.textContent =("Mak everthhing bold")
+    button.addEventListener("click",makeParagraphsBold)
+    const article = getFirstEelment ("article")
+    article.appendChild(button)
+ }
 
+ function hideH3(){
+    h3?.classList.add("hide")
+}
 
+function changeBackground(){
+   h2?.classList.add("red")
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// // const h1 = getFirstEelment("h1")
-// // const h2 = getFirstEelment("h2")
-// // const h3 = getFirstEelment("h3")
-// // const secondParagraph = getSecondParagraph()
-// // removeLastLine()
-// // addButton()
-
-
-// // h2?.addEventListener("click", changeBackground)
-// // h3?.addEventListener("click", hideH3)
-// // h1?.addEventListener("mouseover", randomizeH1size)
-// // secondParagraph.addEventListener("mouseover", makeSecondParagraphFadeout)
-
-// // function makeSecondParagraphFadeout(){
-// //     secondParagraph.classList.add("faeout")
-// // }
-
-
-// // function getSecondParagraph(){
-// //     return document.querySelectorAll("p")[1]
-
-// // }
-
-
-// // function getRandomNumber(){
-// //     return Math.floor(Math.random () * 100 + 1 )
-// // }
-
-// // function randomNumber(){
-// //     const randomNumber = getRandomNumber()
-// //     h1.style . fontSize = randomNumber + "px" 
-// // }
-// // function makeParagraphsBold(){
-// //     const paragraphs = document.querySelectorAll("px")
-// //     for (const paragraph of paragraphs){
-// //         paragraph.classList.add(Blob)
-// //     }
-// // }
-
-
-// // function addButton(){
-// // }
-
-// // function hideH3(){
-// //     h3?.classList.add("hide")
-// // }
-
-// // function changeBackground(){
-// //     h2?.classList.add("red")
-// // }
-
-// // function removeLastLine(){
-// //     const article = document.querySelector("article")
-// //     article?.lastElementChild.remove()
-// // }
-// // function getFirstEelment(querySelector){
-// //     return document.querySelector
-// // }
+function removeLastLine(){
+    const article = document.querySelector("article")
+    article?.lastElementChild.remove()
+}
+function getFirstEelment(Selector){
+     return document.querySelector(Selector)
+}
 
 
 

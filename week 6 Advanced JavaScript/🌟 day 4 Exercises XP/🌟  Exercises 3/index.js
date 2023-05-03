f080a4e91a173a6a29954e904932d768ceb0879b
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-03 22:13:02 +0300

    new

---
+++ week 6 Advanced JavaScript/🌟 day 4 Exercises XP/🌟  Exercises 3/index.js
@@ -0,0 +1,4 @@
+// Create a self invoking function that takes 4 arguments: number of children,
+//  partner’s name, geographic location, job title.
+// The function should display in the DOM a sentence like "You will be a 
+// <job title> in <geographic location>, and married to <partner's name> with <number of children> kids."

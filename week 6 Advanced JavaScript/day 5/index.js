3d47733450c69676bc047dd8c09d14e048333c2e
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-04 20:36:31 +0300

    new

---
+++ week 6 Advanced JavaScript/day 5/index.js
@@ -0,0 +1,15 @@
+//document.getElementById("Quote”").innerHTML = Math.random();console.log(Math.expm1(0));
+// Expected output: 0
+
+console.log(Math.expm1(1));
+// Expected output: 1.718281828459045
+
+console.log(Math.expm1(-1));
+// Expected output: -0.6321205588285577
+
+console.log(Math.expm1(2));
+// Expected output: 6.38905609893065
+
+function displayResult() {
+    document.getElementById("myHeader").innerHTML = "Have a nice day!";
+  }

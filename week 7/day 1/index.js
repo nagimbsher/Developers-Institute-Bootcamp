a7a43d1c0bbe9e20d3ba606217ebcf007e43fcdc
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-08 19:31:01 +0300

    new

--- week 7/day 1/index.js
+++ week 7/day 1/index.js
@@ -116,18 +116,19 @@ function birthdayCakeCandles(arr) {
 }
 
 
-
-
-
-
-
-
 /*
 * findIntersection function return an array that
 * contain the numbers exist in both strings from the array.
 * ["1,2,5,6,7", "2,5,7,8,15"] => [2,5,7]
 */
-
+function findIntersection (arr){
+    let arr1 = arr[0].split(',') //[1,2,5,6,7]
+    let arr2 = arr[1].split(',')//[2,5,7,8,15]
+    let ret = arr1.filter(value =>{
+        return arr2.includes(value)
+    })
+return ret
+}
 
 
 

c17c49a6ced123bff1db9b376599653763166409
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-10 23:06:21 +0300

    new

---
+++ week 7/🌟 Exercise XP day 4/🌟 Exercise 2/index.js
@@ -0,0 +1,15 @@
+//Using the code above, destructure the parameter inside the function and return a string as the example seen below:
+//output : 'Your full name is Elie Schoppik'
+
+//destructuring
+
+
+function displayStudentInfo(objUser){
+    const{first,last} = objUser
+    return `your first name ${first} and your last name is ${last}`;
+}
+
+const sentence = displayStudentInfo({first: 'Elie', last:'Schoppik'});
+console.log("sentence:",sentence);
+
+

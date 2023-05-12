972d0df01460d69b1a525095ad61c953173fb2e5
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-12 10:13:25 +0300

    new

--- week 7/🌟 Exercise XP day 4/🌟 Exercise 4/index.js
+++ week 7/🌟 Exercise XP day 4/🌟 Exercise 4/index.js
@@ -1,6 +1,6 @@
 //Exercise 4 : Person Class
 //Instructions
-//Analyze the code below. What will be the output?
+//1-Analyze the code below. What will be the output?
 class Person {
   constructor(name) {
     this.name = name;

ec58e18d19d44c7fa0c2b14f40fc535f3f4cf1ec
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-12 10:14:22 +0300

    new

---
+++ week 7/🌟 Exercise XP day 4/🌟 Exercise 3/index.js
@@ -0,0 +1,10 @@
+// 🌟 Exercise 3: User & Id
+// Instructions
+// Using this object const users = { user1: 18273, user2: 92833, user3: 90315 }
+
+// Using methods taught in class, turn the users object into an array:
+// Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+// FYI : The number is their ID number.
+
+// Modify the outcome of part 1, by multipling the user’s ID by 2.
+// Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]

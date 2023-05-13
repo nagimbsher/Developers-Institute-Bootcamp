883d6d4097761ae29bd4c231e3b90e5c3d02e33d
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-13 04:04:37 +0300

    new

--- week 7/🌟 Exercises XP day 4/🌟 Exercise 5/index.js
+++ week 7/🌟 Exercises XP day 4/🌟 Exercise 5/index.js
@@ -9,11 +9,11 @@ class Dog {
 };
 //Analyze the options below. Which constructor will successfully extend the Dog class?
   // 1
-class Labrador extends Dog {
-  constructor(name, size) {
-    this.size = size;
-  }
-};
+//class Labrador extends Dog {
+  //constructor(name, size) {
+    //this.size = size;
+  //}
+//};
 
 
   // 2
@@ -24,20 +24,19 @@ class Labrador extends Dog {
   }
 };
 
-
   // 3
-class Labrador extends Dog {
-  constructor(size) {
-    super(name);
-    this.size = size;
-  }
-};
+//class Labrador extends Dog {
+  //constructor(size) {
+    //super(name);
+    //this.size = size;
+  //}
+//};
 
 
   // 4
-class Labrador extends Dog {
-  constructor(name, size) {
-    this.name = name;
-    this.size = size;
-  }
-};
+//class Labrador extends Dog {
+  //constructor(name, size) {
+    //this.name = name;
+    //this.size = size;
+  //}
+//};

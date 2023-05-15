b7d0a221c9e72b6041dc1bb278beb66e695a5435
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-15 18:04:20 +0300

    nagim

---
+++ week 8  Advanced JavaScript/HTTP & FORM Method GET & POST/form.elements/index.js
@@ -0,0 +1,8 @@
+let form = document.forms[0];
+let ageElems = form.elements.age;
+console.log(ageElems)
+RadioNodeList(2)
+ //0: input
+// 1: input
+ //length: 2
+console.log(ageElems[0].value); // "10"

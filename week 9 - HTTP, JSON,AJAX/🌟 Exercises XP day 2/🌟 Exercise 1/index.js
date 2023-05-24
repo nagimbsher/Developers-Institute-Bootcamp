93e6c5420ea5a72474b304e57e92ee6a0d633c2a
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-24 07:44:53 +0300

    new

--- week 9 - HTTP, JSON,AJAX/🌟 Exercises XP day 2/🌟 Exercise 1/index.js
+++ week 9 - HTTP, JSON,AJAX/🌟 Exercises XP day 2/🌟 Exercise 1/index.js
@@ -1,9 +1,29 @@
+// 1-Create a function called compareToTen(num) that takes a number as an argument.
+//2- The function should return a Promise:
+// the promise resolves if the argument is less than 10
+// the promise rejects if argument is greater than 10.
+// Test:
+
+//In the example, the promise should reject
+compareToTen(15)
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+//In the example, the promise should resolve
+compareToTen(8)
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+
+
+
+
 function compareToTen(num){
     return new Promise((resolve,reject) =>{
 if (num <10){
-    resolve("Number is small then 10 .A");
+    resolve("Number is small then 10 .All good");
 }else{
-("Number is too hight.try again");
+reject("Number is too hight.try again");
 }
     });
 }
---
+++ week 9 - HTTP, JSON,AJAX/🌟 Exercises XP day 2/🌟 Exercise 4/index.js
@@ -0,0 +1,21 @@
+// Exercise 4: Quizz - Not Mandatory
+// Follow this 
+("https://www.codingame.com/playgrounds/347/javascript-promises-mastering-the-asynchronous/what-is-asynchronous-in-javascript")
+//tutorial and do the quizz until the page called 
+//“a few tricks with promises”.
+
+
+// let counter = 0;
+
+// let timer = setInterval(function() {
+//     console.log('I am an asynchronous message');
+
+//     counter += 1;
+
+//     if (counter >= 5) {
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
+// console.log('I am a synchronous message');
+

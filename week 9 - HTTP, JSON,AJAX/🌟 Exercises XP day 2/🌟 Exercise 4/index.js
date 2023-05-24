6a27ee7dbb020a4aecd637afefa8b5a298d9a3b7
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-24 07:50:06 +0300

    new

--- week 9 - HTTP, JSON,AJAX/🌟 Exercises XP day 2/🌟 Exercise 4/index.js
+++ week 9 - HTTP, JSON,AJAX/🌟 Exercises XP day 2/🌟 Exercise 4/index.js
@@ -19,3 +19,20 @@
 
 // console.log('I am a synchronous message');
 
+setTimeout(function() {
+    console.log('I am an asynchronous message');
+}); // You can omit the 0
+
+console.log('Test 1');
+
+for (let i = 0; i < 10000; ++i) {
+    doSomeStuff();
+}
+
+console.log('Test 2');
+
+// The 'I am an asynchronous message' will be displayed when the main thread reach here
+
+function doSomeStuff() {
+    return 1 + 1;
+}

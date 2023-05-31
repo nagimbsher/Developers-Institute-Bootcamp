cda2995f9e56669a9eca19872d1d7f1ee4630aea
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-05-31 06:54:14 +0300

    new

---
+++ week 3 - JavaScript/Simple Ways To Speed Up Learning/index.js
@@ -0,0 +1,71 @@
+// 6 Simple Ways to Speed Up Learning a Programming Language
+
+// In today’s world, you can find hundreds of articles on anything. All the required information is available online, maybe not properly organized. It is best to stick with official documentation, but sometimes the knowledge can be way over the head.
+
+// Many beginners find it difficult to learn a language without paying for online courses. In reality, it is not required to enroll in some paid courses to learn to program.
+
+// Here, six simple yet effective techniques to speed up learning a programming language on your own.
+
+// Back To Basics
+// Learning works like investing. It starts with smaller portions, but consistent efforts compound it over the time period.
+// People often rush to advance concepts while learning a programming language. Many usually oversee or skip the basics by not understanding the importance. This later hurts them as everything eventually comes back to basics.
+
+// Spend most of your learning time on basics as they’ll be creating building blocks for your learning. Good programmers have their basics clear, and that’s why they don’t struggle compared to others.
+
+// If your fundamentals are strong, then you will find it easier to grasp advanced topics. Directly jumping to these topics won’t help you in understanding the concept.
+
+// Learn The Right Way
+// You cannot expect to learn programming by reading about variables, methods, and loops. Fold your sleeves and start coding right from the start.
+
+// Before learning a language, setup the environment on your machine for coding. Be it online or download the free edition, but you have to practice every concept by implementing it on your own.
+
+// Reading is not the same as understanding, and actually trying to write the code yourself, or at least running it, will facilitate the learning process much more.
+
+// When you start coding, practice the same code or sample again and again until or unless you don’t need to refer to the same book or resource from where you have learned.
+
+// Play With Debugger
+// A debugger is the first and foremost mentor of any programmer. It teaches you a lot more than anyone can about coding.
+
+// Try to debug as much as possible to understand how things work. It gives you insight into the concepts in a practical manner. Also, you will find a lot of errors in your code at the beginning, so it’s good to use debuggers to find out errors and learn how to fix them.
+
+// If you are good at debugging, it will be easier to learn to program. So learn to use some good debugging technique or use tools to check your piece of code.
+
+// Everyone Needs Someone
+// It holds for programming as well. Sometimes, people find it hard to be consistent in their journey to learn to program. In that case, you can find your coding buddy to sail right through.
+
+// There are a lot of online communities that help people to find coding partners with whom they can code and learn. You can start coding with these communities to build your network —
+
+// CodeBuddies
+// FreeCodeCamp
+// Apart from these, there are several Reddit pages where people ask to collaborate.
+// Sharing your knowledge and discussing topics is one of the best ways to learn faster. You can speed up learning a language by finding a partner as it will keep you motivated throughout the journey.
+
+// Avail Your Powers
+// When I first started programming, I was often dependent on others whenever I stuck in the code. In today’s world, when every information is available online, we don’t have to depend on others. We only need to learn how to extract that information.
+
+// There are several websites like StackOverflow, CodeProject, and Github that helps programmers to find solutions to their problems.
+// Once you master how to use these resources, then you can easily find solutions to most of your problems. It helps you to achieve a sense of independence as you won’t be depending on others for your work.
+
+// Code To Solve
+// Don’t think your work is over once you code to understand a concept. Solve problems available online to test your learnings.
+// Many professional programmers solve online problems to test their capabilities. It helps you to think and code better. There are several websites with competitive coding problems —
+
+// LeetCode
+// HackerRank
+// CodeChef
+// TopCoder
+// Some of the problems mentioned in the above resources have already occurred as interview questions in companies like Google, Facebook, and Amazon.
+
+// Key Takeaways
+// In sum, we can learn programming in an infinite number of ways, given so many resources available online. We can utilize the below-given techniques to speed up learning a programming language —
+
+// Spend more time understanding the fundamentals of the language. It will help you to grasp advanced concepts later on.
+// Learn by coding from the start instead of just reading about the concepts.
+// Fiddle your code with the debugger and understand how it works. Debugging can teach you a lot about coding.
+// Find code buddies for your side projects in online communities. It will keep you motivated to code in the long run.
+// Utilize online resources to find the solution instead of depending on others for the same.
+// Give time to competitive coding to build strong logic. It will keep you ahead in the game.
+// By taking these small actions, anyone can speed up their learning process. Programming cannot be mastered in a small time period. It is a practice of years that builds up your experience.
+
+// Make sure to start learning the language in the right way. These techniques can help you to get started faster and deep dive into the programming world.
+

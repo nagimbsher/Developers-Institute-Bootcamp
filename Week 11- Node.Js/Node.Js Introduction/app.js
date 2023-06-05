f5d3b2045026884bb8ba9aafd7e4447b53084bac
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-05 07:15:11 +0300

    new

---
+++ Week 11- Node.Js/Node.Js Introduction/app.js
@@ -0,0 +1,27 @@
+
+const  http = require ('http');
+const sevrver  = http .createServer
+
+
+// 7
+// 1. create a module users.js
+// 2. create a function that get the json user file
+// https://jsonplaceholder.typicode.com/users
+// 3. return the arr of users from the function
+// 4. console log the arr of user in
+// the file that you require it (not in the module
+
+//1.creat a new folder 
+//npm init -y
+//2.insatal package - slugify from 
+//3. check node_modules _ dependencies
+//4.make a simple examle with with slugify
+
+// 1. create a new folder
+// npm init -y
+// 2. insall package - slugify from npm
+// 3. check node_modules - dependencies
+// 4. make a simple examle with slugify
+
+// install nodemon
+// run nodemon

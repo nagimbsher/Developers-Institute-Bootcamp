2e0201d37194f1bcc62a256eaeebd1d93adb6cff
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-19 20:08:15 +0300

    new

---
+++ Week 11- Node.Js/🌟 Exercises XP Day 5/Daily Challeng/node/login.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+const messageBox = document.getElementById("message-box");
+const[username, password] =["username", "password"].map((id)=>
+document.getElementById(id));
+
+const url = "http://localhost:3000/login"; 
+
+form?.addEventListener("submit",handleSubmit);
+
+
+function handleSubmit(e) {
+    e.preventDefault();
+    if(username.value === ""|| password.value === "") return;
+    const options = {
+        method: "POST",
+        Headers:{
+            "Conten-Type":"appliction/json"
+        },
+        body: JSON.stringify({password:password.value, username:
+        username.value})
+    }
+
+    .fetch(url, options)
+    .then ((res) => res.json())
+    .then ((res) => (messageBox.textContent = res.message))
+    .catch(console.error)
+    .finally(() => {
+    [username, password].forEach((input) => (input.value = ""));
+    });
+}
---
+++ Week 11- Node.Js/🌟 Exercises XP Day 5/Daily Challeng/node/register.js
@@ -0,0 +1,45 @@
+const ids = ["first","last","email","username","password"];
+const divs = ids.map((id) => document.getElementById(id));
+console.log("divs:",divs);
+
+// const submitButton = document.getElementById("submit");
+// const form = document.querySelector("form");
+// const messageBox = document.getElementById("message");
+// form?.addEventListener("submit", handleSubmit);
+// divs.forEach((divs) => divs?.addEventListener("input",
+// handleSubmit));
+
+
+// const url = "http:localhost:3000/register";
+
+// function handleSubmit(e){
+//     e.preventDefault();
+//     const [first, last, email, username, password] = divs.map
+//     ((div) => div.value);
+//     const body =[first, last, email, username, password];
+//     const options ={
+//         headers:{
+//         "conten-Typ":"application/j"
+//     },
+//     method: "POST",   
+//     body:JSON.stringify(body)
+// };
+// fetch(url, options)
+//  .then((res) => res.json())
+//  .then((res) =>(messageBox.innerText = res.message))
+//  .catch(console.error)
+//  .finally(() => { form.error()
+//     divs .forEach((div) => (div.value = ""));
+
+//  }
+
+//  )};
+ 
+// function handleChange(e){
+//     submitButton.disabled = isAnyFieldEmpty();
+
+// }
+
+// function isAnyFieldEmpty (){
+//     return divs.some((div) => div === "");
+// }

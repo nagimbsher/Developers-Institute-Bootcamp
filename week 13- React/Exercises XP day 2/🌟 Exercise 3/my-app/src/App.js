71ae27a26b5a41cb7f275103e1e1445d7e606085
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-20 07:27:13 +0300

    new

---
+++ week 13- React/Exercises XP day 2/🌟 Exercise 3/my-app/src/App.js
@@ -0,0 +1,48 @@
+
+export default function App(){
+const user = {
+  firstName: 'Bob',
+  lastName: 'Dylan',
+  favAnimals : ['Horse','Turtle','Elephant','Monkey']
+};
+return(
+  <>
+  <h3>{user.firstName}</h3>
+  <h3>{user.lastName}</h3>
+ < UserFavoriteColors favorites = {user.favAnimals}/>
+  </>
+);
+
+}
+
+function UserFavoriteColors(props) {
+    const favorites = props.favorites;
+    console.log("favorites:", favorites);
+  return favorites.map((favorite,i) =><li key= {i}>{favorite}</li>);
+}
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;

6656068c6c59d92a61e6fb17990ddfe9d5bbcbcd
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-27 08:27:21 +0300

    new

--- week 13- React/roobo/my-app/src/App.js
+++ week 13- React/roobo/my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react'
+import 'tachyons'
+import users from './users.json'
+export default class User extends Component {
+    render (){
+    return (
+      <div className='User'>
+        {
+      users.map( (item,indx) => {
+        return(
+        <User info={item} key={indx}/>
+      )
+    })
+      }
     </div>
   );
 }
+  }
 
-export default App;
+
+

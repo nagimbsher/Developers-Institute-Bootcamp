c41d9052374473b73fffe5634a0631687dbcca06
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-28 22:23:32 +0300

    new

--- week - 14/Exercises XP day 2/Exercises 1/my-app/src/App.js
+++ week - 14/Exercises XP day 2/Exercises 1/my-app/src/App.js
@@ -18,3 +18,37 @@ class App extends Component {
   }
 }
 export default  App
+
+// import React, { Component } from "react";
+
+// class BuggyCounter extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       counter: 0
+//     };
+//   }
+
+//   handleClick() {
+//     this.setState({ counter: this.state.counter + 1 });
+//     this.setState({ counter: this.state.counter + 1 });
+//     this.setState({ counter: this.state.counter + 1 });
+//     this.setState({ counter: this.state.counter + 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Counter</h1>
+//         <span>current state: {this.state.counter}</span>
+//         <br />
+//         <button onClick={this.handleClick}>click me!</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default BuggyCounter;
+
+
+

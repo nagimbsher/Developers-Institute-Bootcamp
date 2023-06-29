2ddbe946837c287747983435c52aedfea2b25049
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-06-29 20:25:38 +0300

    new

--- week - 14/Daily Challenge/my-app/src/components/UserForm.componen.js
+++ week - 14/Daily Challenge/my-app/src/components/UserForm.componen.js
@@ -1,18 +1,91 @@
 import React from "react";
 export class UserForm extends React.Component{
     constructor(props) {
-        console.log("props:",props);
         super(props);
+    }
+    handleSubmit = (e) =>{
+    e.preventDefault();
+    const entries = Object.entries(this.props.info);
+    const params =  entries.jon("8").replace(/,/g/"=")
+    const url = "http://localhost/?"+params;
+    fetch(url);
+    };
+    render(){
+        const destination = ["Japan","Thailand","Brazil","Israel","UK","Spain"];
+        return(
+            <>
+            <form id="onSubmit={this.handleSubmit}">
+                <input
+                type="text"
+                name="firstname"
+                value={this.props.info.firstName}
+               onChange={(e) => this.props.ChangeLastName(e.target.value)} 
+               />
+                <input
+                type="text"
+                name="age"
+                placeholder="Age"
+                value={this.props.info.age}
+               onChange={(e) => this.props.changeAge(e.target.value)}
+                />
+                <input
+                type="radio"
+                name="male"
+                value="male"
+                id="gender1"
+                checked={this.props.info.gender === "male"}
+                onChange={(e) => this.props.ChangeGender(e.target.value)}
+                />
+                <label htmlFor="gender1">Mele</label>
+                <input
+                type="text"
+                name="lastname"
+                value={this.props.info.lastName}
+                onChange={(e) => this.props.ChangeLastName(e.target.value)} 
+                />
+                <label htmlFor="gender2">Famale</label>
+                <input
+                type="radio"
+                name="female"
+                value="female"
+                id="gender2"
+                checked={this.props.info.gender==="female"}
+                onChange={(e) => this.props.ChangeGender(e.target.value)}
+                />
+                <label htmlFor="">Destination</label>
+                <select 
+                name="destination"
+                id="destination"
+                onChange={(e) => this.props.ChangeDestination(e.target.value)}
+                value={this.props.destination}
+                >
+                    <option value="Thailand">Thailand</option>
+                    <option value="Japan">Japan</option>
+                    <option value="Brazil">Brazil</option>
+                </select>
+                <p> Dietary restrictions</p>
+                <input
+                checked={this.props.info.noNuts}
+                type="checkbox"
+                name="nonuts"
+                id="diet1"
+                onChange={(e) => this.props.changeNonuts(e.target.checked)}
+                ></input>-
+
+            </form>
+            </>
+
+        );
     }
     render(){
         return<>
         <form method="post" action="/" id="form">
-            <input type ="text" name= "firstname" placeholder="First name" />
-            <input type ="text" name= "lastname" placeholder="Last name" />
-             <input type ="text" name= "age" placeholder="age" />
+            <input type ="text" name= "firstname" value={this.props.info.firstName} />
+            <input type ="text" name= "lastname" value={this.props.info.lastName} />
+             <input type ="text" name= "age" placeholder="Age" value={this.props.info.age} />
 
              <label htmlFor="gender">Mele</label>
-           <input type="radio" name="male" value="male" id="gender1"/>
+           <input type="radio" name="male" value="male" id="gender1" defaultChecked/>
            <label htmlFor="gender">Famale</label>
            <input type="radio" name="famale" value="Famale" id="gender2"/>
 
--- week - 14/Exercises XP day 2/Exercises 1/my-app/src/App.js
+++ week - 14/Exercises XP day 2/Exercises 1/my-app/src/App.js
@@ -1,54 +1,54 @@
 
-import React, { Component } from 'react'
-import  Hero from './components/Hero'
-import ErrorBoundary from './components/ErrorBoundary'
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <ErrorBoundary>
-        <Hero heroName="Batman"/>
-        </ErrorBoundary>
-        <Hero heroName="Superman"/>
-        <ErrorBoundary>
-        <Hero heroName="Joker"/>
-        </ErrorBoundary>
-      </div>
-    )
-  }
-}
-export default  App
-
-// import React, { Component } from "react";
-
-// class BuggyCounter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       counter: 0
-//     };
-//   }
-
-//   handleClick() {
-//     this.setState({ counter: this.state.counter + 1 });
-//     this.setState({ counter: this.state.counter + 1 });
-//     this.setState({ counter: this.state.counter + 1 });
-//     this.setState({ counter: this.state.counter + 1 });
-//   }
-
+// import React, { Component } from 'react'
+// import  Hero from './components/Hero'
+// import ErrorBoundary from './components/ErrorBoundary'
+// class App extends Component {
 //   render() {
 //     return (
-//       <div>
-//         <h1>Counter</h1>
-//         <span>current state: {this.state.counter}</span>
-//         <br />
-//         <button onClick={this.handleClick}>click me!</button>
+//       <div className='App'>
+//         <ErrorBoundary>
+//         <Hero heroName="Batman"/>
+//         </ErrorBoundary>
+//         <Hero heroName="Superman"/>
+//         <ErrorBoundary>
+//         <Hero heroName="Joker"/>
+//         </ErrorBoundary>
 //       </div>
-//     );
+//     )
 //   }
 // }
+// export default  App
 
-// export default BuggyCounter;
+import React, { Component } from "react";
+
+class BuggyCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <span>current state: {this.state.counter}</span>
+        <br />
+        <button onClick={this.handleClick}>click me!</button>
+      </div>
+    );
+  }
+}
+
+export default BuggyCounter;
 
 
 

cfb4e6e0e869c2c2222ecebf19b39fcd3a3c5eed
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-07-01 00:17:50 +0300

    updata

--- week - 14/Exercises XP day 4/Exercises 4/my-app/src/App.js
+++ week - 14/Exercises XP day 4/Exercises 4/my-app/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { ErrorBoundary } from "./components/ErrorBoundary.component";
 import "bootstrap/dist/css/bootstrap.min.css"
-export class App extends React.Component {
 
+
+export class App extends React.Component{
     handleClick =async() =>{
       const url = "https://webhook.site/1142be32-e5c4-4a02-bb79-2a0ebe9903c7";
       try{
-      const res = await fetch(url);
+        //const res = await fetch(url);
+        const body = {key1: 'myusername',
+        email: 'mymail@gmail.com',
+        name: 'Isaac',
+        lastname: 'Doe',
+        age: 27}
       const options = {
         method:"POST",
         Headers:{
@@ -14,21 +20,23 @@ export class App extends React.Component {
         },
         body:JSON.stringify(body)
         };
-        const res = await fetch(url, options);
-        const res = await res.json();
-        console.log("res:",res)
+        const res = await fetch(url,options);
+        const resJson= await res.json();
+        console.log("resJson:",resJson)
       } catch(err){
         console.error(err);
       };
+    }
     render(){
     return (
       <ErrorBoundary>
         <button onClick={this.handleClick}> Post Data</button>
       </ErrorBoundary>
+    
     );
-  }
-}  
-}
+  };
+} ; 
+
 
    export default(App)
    

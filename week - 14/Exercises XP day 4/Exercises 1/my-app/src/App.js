c919252590e46c12dc0f94cfe69da1bc3794d36d
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-07-06 07:13:17 +0300

    new

--- week - 14/Exercises XP day 4/Exercises 1/my-app/src/App.js
+++ week - 14/Exercises XP day 4/Exercises 1/my-app/src/App.js
@@ -1,57 +1,123 @@
-import React from 'react'
-import { ErrorBoundary } from './components/ErrorBoundary.component'
-import{ Routes, Route, NavLink , BrowserRouter} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import React, { Component } from "react";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import { Routes, Route, NavLink, BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 const routes = (
-  <Routes> 
+  <Routes>
     <Route path="/" element={<HomeScreen />}></Route>
     <Route path="/shop" element={<ShopScreen />}></Route>
-    <Route path="/profile" element={<profileScreen />}></Route>
+    <Route path="/profile" element={<ProfileScreen />}></Route>
   </Routes>
 );
-
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <ErrorBoundary>
-      <BrowserRouter >
-      <Navbar/>
-      {routes}
-     </BrowserRouter >
-     </ErrorBoundary>
+
+        <BrowserRouter>
+          <Navbar />
+          {routes}
+        </BrowserRouter>
+
     );
   }
 }
-
 function Navbar() {
-  return(
-     <> 
-    <h1>Welcome to React Router!</h1>
-    <p>
-  <NavLink to="/">Home</NavLink>
-  </p>
-  <p>
-  <NavLink to="/profile">Profile</NavLink>
-  </p>
-  <p>
-  <NavLink to ="/shop">Shop </NavLink>
-  </p>
-  </>
+  return (
+    <>
+      <h1>Welcome to React Router!</h1>
+      <p>
+        <NavLink to="/">Home</NavLink>
+      </p>
+      <p>
+        <NavLink to="/profile">Profile</NavLink>
+      </p>
+      <p>
+        <NavLink to="/shop">Shop </NavLink>
+      </p>
+    </>
   );
 }
 
-function HomeScreen (){
-  return<h1> Home </h1>;
+function HomeScreen() {
+  return (
+    <>
+      <h1>HomeScreen</h1>
+    </>
+  );
 }
- function ShopScreen (){
-  return<h1> Shop</h1>;
- }
 
- function profileScreen(){
+function ShopScreen() {
+  return (
+    <>
+      <h1>ShopScreen</h1>
+    </>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <>
+      <h1>profileScreen</h1>
+    </>
+  );
+}
+
+
+
+//import React from 'react'
+// import { ErrorBoundary } from './components/ErrorBoundary.component'
+// import{ Routes, Route, NavLink , BrowserRouter} from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css"
+
+// const routes = (
+//   <Routes> 
+//     <Route path="/" element={<HomeScreen />}></Route>
+//     <Route path="/shop" element={<ShopScreen />}></Route>
+//     <Route path="/profile" element={<profileScreen />}></Route>
+//   </Routes>
+// );
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <ErrorBoundary>
+//       <BrowserRouter >
+//       <Navbar/>
+//       {routes}
+//      </BrowserRouter >
+//      </ErrorBoundary>
+//     );
+//   }
+// }
+
+// function Navbar() {
+//   return(
+//      <> 
+//     <h1>Welcome to React Router!</h1>
+//     <p>
+//   <NavLink to="/">Home</NavLink>
+//   </p>
+//   <p>
+//   <NavLink to="/profile">Profile</NavLink>
+//   </p>
+//   <p>
+//   <NavLink to ="/shop">Shop </NavLink>
+//   </p>
+//   </>
+//   );
+// }
+
+// function HomeScreen (){
+//   return<h1> Home </h1>;
+// }
+//  function ShopScreen (){
+//   return<h1> Shop</h1>;
+//  }
+
+//  function profileScreen(){
   
-  return <h1> Profile</h1>;
- }
+//   return <h1> Profile</h1>;
+//  }
 
 
  
--- week - 14/day 4 Daily Challenge/id/my-app/src/App.js
+++ week - 14/day 4 Daily Challenge/id/my-app/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-
+import "./App.css";
+import heroes from "./data/heroes.json";
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {heroes:heroes.superheroes};
+  }
   render() {
     return (
       <div className='container'>
-        <nav className=''>
-
+        <nav className='navbar'>
+<h1>Superheroes React Memory Game</h1>
+<div> Score:0</div>
+<div>TOP Scour:0</div>
         </nav>
-      <nav>Superheroes React Memory Game</nav>
+        <div className='rules'>
+          Get points by clicking on an image but do not click the same one twice
+        </div>
+        <div className='heroes-container'> 
+        {this.state.heroes.map((hero) =>{
+          return<div className='hero-card'>{hero.name}</div>
+        })}
+        </div>
       </div>
-    )
+    );
   }
 }

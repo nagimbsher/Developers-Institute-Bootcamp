6912c7d505d18a1133c15aa1c2bfd49aa8b284a5
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-07-25 03:50:39 +0300

    updata

--- week 13- React/min-project/nagim-app/src/App.js
+++ week 13- React/min-project/nagim-app/src/App.js
@@ -1,19 +1,40 @@
-import React, { Component} from 'react'
-import "./App.css";
+import "./styles.css";
+import { useState, useEffect } from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      
-      <div className='box'>
-         <h1>A box her</h1> 
-         <h2> Thomas Edison</h2>
-         <div className='button-container'>
-          <button> New Quote</button>
-         </div>
-         </div>
-    );
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
   }
+
+  return (
+    <main>
+      <h1>Project 3: Quote Generator</h1>
+      <section>
+        <button onClick={getNewQuote}>New Quote</button>
+        <h3>
+          <span>“</span>
+          {quote?.text}
+        </h3>
+        <i>- {quote?.author}</i>
+      </section>
+    </main>
+  );
 }
 
 
